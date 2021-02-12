e40b26598c060fda39187aea8a197727dc2507ce
Roger Bucio <335515@colum.edu>
2021-02-12 14:32:09 -0600

    Addition of Programmed functions

---
+++ Assets/Script/Interfaces.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IDamageable<T>
+{
+    void HealDamage(T damageHealed);
+    void TakeDamage(T damageTaken); //T will be int
+    void OnDamage();
+}
+
+public interface IKillable
+{
+    void Kill();
+    void OnKill();
+}
+
+public interface IBuyable<T>
+{
+    void Buy(T buyPrice);
+    void Sell(T sellPrice);
+}
+
+public interface IRollable
+{
+    //prolly not needed, only dice are rollable
+}
